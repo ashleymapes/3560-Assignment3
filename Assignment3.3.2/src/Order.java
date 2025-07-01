35b205783e66c74745518bf633e3854d038b5ff5
ashleymapes <amapes05@gmail.com>
2025-07-01 00:38:54 -0700

    added basic stuff

--- Assignment3.3.2/src/Order.java
+++ Assignment3.3.2/src/Order.java
@@ -1,3 +1,49 @@
-public class Order {
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order")
+public class Order{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "customer_name")
+    private String customer_name;
+
+    @ManyToMany(cascade={CascadeType.PERSIST})
+    @JoinTable(
+        name="order_detail",
+        joinColumns = @JoinColumn(name="order_id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id")
+
+    )
+
+    private List<Product> products;
+
+    public Order(){}
+
+    
+
 
 }
--- Assignment3.3.2/src/Product.java
+++ Assignment3.3.2/src/Product.java
@@ -1,3 +1,40 @@
-public class Product {
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "products")
+
+    private List<Order> orders;
+
+    public Product(){}
+
+    
+
 
 }
